1a9fb33255a4c4fe07b651bac7663f18c6451eec
x23176971 <167620296+x23176971@users.noreply.github.com>
2024-04-20 14:02:01 +0100

    Add files via upload
    Server with unary and bidirectionnal

--- server.js
+++ server.js
@@ -11,8 +11,47 @@ const NoRobberyProto = grpc.loadPackageDefinition(packageDefinition).NoRobbery;
 // Creating gRPC server
 const server = new grpc.Server();
 
+const rooms = ['living room', 'bedroom', 'kitchen', 'bathroom'];
 
+// Adding services to the server
+server.addService(NoRobberyProto.NoRobbery.service, {
+    // Service method to set music timer
+    SetTimeMusic: (call, callback) => {
+        // Extracting desired time from the request
+        const desiredTime = call.request.time;
+        // Logic to set the time, e.g., interact with the security system
+        // For now, let's just return a confirmation message
+        const message = `Time set to ${desiredTime} minutes\n`;
+        // Sending response back to the client
+        callback(null, { message });
+        console.log(`The client has set the time to ${desiredTime}\n`);
+    },
 
+    // Service method to activate alarm
+    activateAlarm: (call) => {
+        console.log("*****Alarm will now turn on for 10 seconds...*****\n");
+        // Sending initial message to the client
+        call.write({ message: 'Alarm activated successfully.\n' });
+
+        // Simulating alarm activation process
+        let countdown = 10;
+        let bipNumber = 1;
+        const countdownInterval = setInterval(() => {
+            if (countdown > 0) {
+                call.write({ message: 'Bip ' + bipNumber });
+                bipNumber++;
+                console.log(`Countdown: ${countdown} seconds remaining...`);
+                countdown--;
+            } else {
+                clearInterval(countdownInterval);
+                console.log("Alarm deactivated.\n");
+                // Ending the call
+                call.end();
+            }
+        }, 1000); // Countdown interval of 1 second
+    },
+
+});
 
 // Binding and starting the server
 server.bindAsync('127.0.0.1:50051', grpc.ServerCredentials.createInsecure(), (err, port) => {
