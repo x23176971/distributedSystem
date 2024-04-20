602131380b5a277db01c2edc18086ecd862c5db9
x23176971 <167620296+x23176971@users.noreply.github.com>
2024-04-20 14:04:05 +0100

    Add files via upload
    Client file complete

--- client.js
+++ client.js
@@ -114,7 +114,47 @@ async function main() {
             // Invoking the function to trigger the alarm
             triggerAlarm();
             break;
-        
+        case '3':
+            // Establishing a stream to control lights
+            const stream1 = client.ControlLights((error, response) => {
+                if (error) {
+                    console.error('Error controlling lights:', error);
+                } else {
+                    console.log('Lights controlled successfully:', response.message);
+                    console.log('\nExiting Smart Security System. Goodbye!');
+                }
+                // Closing the readline interface
+                rl.close();
+            });
+
+            // Function to control lights in each room
+            async function controlLights() {
+                for (let i = 0; i < rooms.length; i++) {
+                    const activate = await askQuestion(`Do you want to activate the lights for 5 minutes in the ${rooms[i]}? (Yes/No): `);
+                    stream1.write({ room: rooms[i], activate: activate.toLowerCase() === 'yes' });
+                }
+                stream1.end();
+            }
+
+            // Invoking the function to control lights
+            await controlLights();
+            break;
+        case '4':
+            // Server streaming call for motion detection
+            const motionDetectionStream = client.MotionDetectionStream({});
+
+            // Listening for messages from the server
+            motionDetectionStream.on('data', (response) => {
+                console.log(response.message); // Log the motion detection message received from the server
+            });
+
+            // Handling end event of the streaming call
+            motionDetectionStream.on('end', () => {
+                console.log('Server has finished streaming.'); // Log when server finishes streaming
+                rl.close(); // Close the readline interface
+            });
+
+            break;
         default:
             // Informing about invalid choice
             console.log('Invalid choice. Please select a valid option.');
